28d54d0e3576232c31be708837d27882bf5abb98
Jose De Las Salas <jld03e@gmail.com>
2018-08-13 18:37:30 -0400

    created an untitled doc that i used to test for loop logic to identify best match, resolved that with assistance from tutor, finalized html yesterday, and added JS and ajax calls to html today.

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,7 +1,6 @@
 //4. Your `apiRoutes.js` file should contain two routes:
 //* A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 //* A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-
 var friendsData = require("../data/friends.js");
 
 module.exports=function(app) {
@@ -13,8 +12,9 @@ app.get("/api/friends", function(req, res) {
 
 app.post("/api/friends", function(req, res) {
 
-    tableData.push(req.body);
+    friendsData.push(req.body);
         res.json(true);
+        
+        console.log(friendsData)
   });
-
 };
--- server.js
+++ server.js
@@ -2,19 +2,20 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
 
 console.log(PORT);
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-})
+});
